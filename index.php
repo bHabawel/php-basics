429dcfafed839a98ac6675c514c8038c9ecf9526
bHabawel <rosshabawel12@gmail.com>
2023-12-19 20:26:58 +0800

    adding if-else

--- index.php
+++ index.php
@@ -5,4 +5,14 @@
 
     // isFinished not displaying as it is a bool
     echo $fName . " " . $num1 . " " . $isFinished;
+
+    // Just for readability
+    echo "<br/>";
+
+    if($num1 > 10){
+        echo $num1 . " is greater than " . 10;
+    }else{
+        echo $num1 . " is less than " . 10;
+    }
+
 ?>
